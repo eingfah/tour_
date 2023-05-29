7b5d10457f891562974add38998b18daab58e6cd
eingfah <124545679+eingfah@users.noreply.github.com>
2023-05-29 11:42:08 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var Geo = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หอยป้ายแดง",
+          "type": "restaurant",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            98.34729891386462,
+            7.899794255058879
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หอยป้ายแดงสาขาสอง",
+          "type": "restaurant",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.3713118025949,
+            7.9109206579761775
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ร้านคันทีน",
+          "type": "restaurant",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.35218169243109,
+            7.892780527937234
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ร้านโรตีอันดา",
+          "type": " dessert shop",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            98.37161527116422,
+            7.866327432242315
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หนุมานเวิล",
+          "type": " dessert shop",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            98.35111283359026,
+            7.876755416803292
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ทรีมังกี้",
+          "type": "restaurant",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            98.35163240164707,
+            7.876287070517677
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวีทเดวิลคาเฟ่",
+          "type": " dessert shop",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            98.38069918321929,
+            7.878472167180689
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บอลลี่วู้ด",
+          "type": "restaurant",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            98.3676883387617,
+            7.900106314002386
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "คอฟฟี่อินฟาร์ม",
+          "type": "restaurant",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            98.34928288532609,
+            7.937486778557286
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "โฮเซบราว ภูเก็ต",
+          "type": "restaurant",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            98.36415735107005,
+            7.883681588595195
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
